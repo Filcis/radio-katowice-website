e584e847fe997898f2490d832fc8fa83590b3e2b
Filcis <cislakfilip@gmail.com>
2018-09-10 14:34:51 +0200

    replaced toggle class for dropdown menu with native jquery slideToggle() method

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,21 @@
 'use strict';
 
 $(document).ready(() => {
-
+    // TODO: Organize code as different modules
+    //==================================================
     const dropdown = $('#stream-dropdown');
     dropdown.click((e) => {
         $(e.currentTarget).toggleClass('is-active');
     });
-
+    //==================================================
     const navbarBurgers = $('.navbar-burger');
     navbarBurgers.click((e) => {
         $(e.currentTarget).toggleClass('is-active');
         console.log(e.currentTarget.dataset);
-        $(e.currentTarget.dataset.target).toggleClass('is-active');
+        $(e.currentTarget.dataset.target).slideToggle("slow");
     });
+    //==================================================
 
+    //==================================================
     var carousels = bulmaCarousel.attach();
 })
