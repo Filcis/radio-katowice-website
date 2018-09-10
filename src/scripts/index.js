0ebec92210e12db1aae2b78056abfc08d5a47f4e
Filcis <cislakfilip@gmail.com>
2018-09-10 14:29:56 +0200

    jQuery integration, added dropdown to player

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,18 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', () => {
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-        // Add a click event on each of them
-        $navbarBurgers.forEach(el => {
-            el.addEventListener('click', () => {
-                // Get the target from the "data-target" attribute
-                const target = el.dataset.target;
-                const $target = document.getElementById(target);
-                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                el.classList.toggle('is-active');
-                $target.classList.toggle('is-active');
-            });
-        });
-    }
 
-    var carousels = bulmaCarousel.attach(); // carousels now contains an array of all Carousel instances
-});
+$(document).ready(() => {
+
+    const dropdown = $('#stream-dropdown');
+    dropdown.click((e) => {
+        $(e.currentTarget).toggleClass('is-active');
+    });
+
+    const navbarBurgers = $('.navbar-burger');
+    navbarBurgers.click((e) => {
+        $(e.currentTarget).toggleClass('is-active');
+        console.log(e.currentTarget.dataset);
+        $(e.currentTarget.dataset.target).toggleClass('is-active');
+    });
+
+    var carousels = bulmaCarousel.attach();
+})
