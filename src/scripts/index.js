c817670d4dab801549246fefedde5d0ab048c942
Filcis <cislakfilip@gmail.com>
2018-09-11 09:31:10 +0200

    added scripts and style to tabs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,12 +15,15 @@ $(document).ready(() => {
         $(e.currentTarget.dataset.target).slideToggle("slow");
     });
     //==================================================
-    // const tabs = $('a.tab');
-    // console.log(tabs);
-    // tabs.click((e) => {
-    //     $(e.currentTarget).toggleClass('is-active');
-    //     console.log(e.currentTarget.dataset.target);
-    // });
+    const tabs = $('a.tab');
+    tabs.click((e) => {
+        tabs.each((i, e) => {
+            $(e).parent().removeClass('is-active')
+            $(e.dataset.target).removeClass('is-active');
+        });
+        $(e.currentTarget.dataset.target).addClass('is-active');
+        $(e.currentTarget).parent().addClass('is-active');
+    });
     //==================================================
     var carousels = bulmaCarousel.attach();
 })
