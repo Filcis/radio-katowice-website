8e0acfcd4957a4dd05425f4c6e56da612049d887
Filcis <cislakfilip@gmail.com>
2018-09-26 11:50:54 +0200

    rewriten carousel as mixin, changed styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -26,12 +26,19 @@ $(document).ready(() => {
         $(e.currentTarget).parent().addClass('is-active');
     });
     //==================================================
-    var carousels = bulmaCarousel.attach();
+    const carousel = $('.video-carousel');
+    const carouselItems = carousel.children();
 
-    // $(window).resize(() => {
-    //     if (window.innerWidth >= 1200) {
-    //         $('#main-video-carousel').data('size', 4);
-    //     }
-    // })
+    var nextItem = (el) => el.next().length > 0 ? el.next() : carouselItems.first();
+    var prevItem = (el) => el.prev().length > 0 ? el.prev() : carouselItems.last();
+
+    $('.carousel-toggle').on('click', (e) => {
+        var el = $('.is-ref');
+        if ($(e.currentTarget).data('toggle') === 'next') {
+            var newItem = nextItem(el);
+        } else {
+            var newItem = prevItem(el);
+        }
+    })
 
 })
