838074f842143928aecbddf035e05cb6c1de8072
Filcis <cislakfilip@gmail.com>
2018-10-02 14:48:18 +0200

    added scroll event to make sticky sidebr, added ads placeholders

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,15 @@
 'use strict';
 
-// TODO: Organize code as different modules, create namespaces for different functionalities
-// no need for document.ready
+// TODO: Zorganizowac kod jako oddzielne moduły
 //==================================================
 
+
 (function($) {
+
+    //==================================================
+    // Player popup
+    //==================================================
+
     if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         $('#player-main-call-to-action').click(function(e) {
             e.preventDefault();
@@ -14,29 +19,58 @@
         });
     }
 
+    //==================================================
+    // Dropdown
+    //==================================================
+
     const dropdown = $('#stream-dropdown');
     dropdown.click((e) => {
         $(e.currentTarget).toggleClass('is-active');
         $(e.currentTarget.dataset.target).slideToggle("slow");
     });
+
     //==================================================
+    // Burger
+    //==================================================
+
     const navbarBurgers = $('.navbar-burger');
     navbarBurgers.click((e) => {
         $(e.currentTarget).toggleClass('is-active');
         console.log(e.currentTarget.dataset);
         $(e.currentTarget.dataset.target).toggleClass('is-active');
     });
+
+    //==================================================
+    // Sticky menu
+    //==================================================
+
+    let Adspace = $('#top-add').outerHeight(true);
+    let navbar = $('#main-nav');
+    console.log(Adspace);
+    $(window).scroll((event) => {
+        let windowScrollTop = $(window).scrollTop();
+        if (windowScrollTop >= Adspace) {
+            navbar = $('#main-nav').addClass('is-fixed-top');
+            $('body').addClass('has-navbar-fixed-top');
+        } else {
+            navbar = $('#main-nav').removeClass('is-fixed-top');
+            $('body').removeClass('has-navbar-fixed-top');
+        }
+
+    })
+
     //==================================================
     // TODO: asynchroniczne tooltipy
-    $(".station-list-item").hover(function() {
-        console.log('ajax request');
-        var val = $(this).html();
-        //
-        // $.get('./stations.html'),
-        //     function() {
-        //         //This function is for unhover.
-        //     }
-    });
+    // $(".station-list-item").hover(function() {
+    //     console.log('ajax request');
+    //     var val = $(this).html();
+    //
+    // $.get('./stations.html'),
+    //     function() {
+    //         //This function is for unhover.
+    //     }
+    // });
+
     //==================================================
     // const tabs = $('a.tab');
     // tabs.click((e) => {
@@ -47,6 +81,7 @@
     //     $(e.currentTarget.dataset.target).addClass('is-active');
     //     $(e.currentTarget).parent().addClass('is-active');
     // });
+
     //==================================================
     // const carousel = $('.video-carousel');
     // const carouselItems = carousel.children();
