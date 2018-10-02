779f98f38ebf3c45fe93ddbe2c81d4cb3cd7a49d
Filcis <cislakfilip@gmail.com>
2018-10-02 21:53:42 +0200

    responsive carousel fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,6 +18,17 @@
             return false;
         });
     }
+    const mediaSize = () => {
+        if (window.matchMedia('(min-width: 968px)').matches) {
+            $('.responsive-carousel').addClass('is-4');
+        } else {
+            $('.responsive-carousel').removeClass('is-4');
+        }
+    }
+
+    mediaSize();
+    /* Attach the function to the resize event listener */
+    window.addEventListener('resize', mediaSize, false);
     //==================================================
     // Dropdown
     //==================================================
