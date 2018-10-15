38568575de43096f37e8ae3441f08158fba848d4
Filcis <cislakfilip@gmail.com>
2018-10-15 14:40:55 +0200

    added rest of articles, and additional styling

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -19,6 +19,11 @@
         });
     }
     const mediaSize = () => {
+        if (window.matchMedia('(min-width: 680px)').matches) {
+            $('.responsive-carousel').addClass('is-2');
+        } else {
+            $('.responsive-carousel').removeClass('is-4');
+        }
         if (window.matchMedia('(min-width: 968px)').matches) {
             $('.responsive-carousel').addClass('is-4');
         } else {
