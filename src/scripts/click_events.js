6206db8bfedc6ac44041eb82efa16a7d38118d8d
Filcis <cislakfilip@gmail.com>
2018-11-10 18:38:18 +0100

    new styles and script for main navigation

--- src/scripts/click_events.js
+++ src/scripts/click_events.js
@@ -7,6 +7,22 @@ let goToTop = (event)=> {
   }, 500);
 }
 
+let query = Modernizr.mq('(max-width: 900px)');
+let dropdown = $('.navbar-link');
+let submenus = $('#main-menu .navbar-dropdown');
+
+//NAVIGATION
+
+
+export function initNav() {
+  if(query){
+    dropdown.on('click', function() {
+        $(this).siblings('.navbar-dropdown').toggleClass('open');
+      });
+  }
+}
+
 export let attachClickEvents = () => {
   goToTopButton.on('click', goToTop);
+  initNav();
 }
