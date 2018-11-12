bff7f3d534ec628f0166c614d12c7714349b3067
Filcis <cislakfilip@gmail.com>
2018-11-12 13:00:05 +0100

    more readable code to switch mq for carousel

--- src/scripts/media_size.js
+++ src/scripts/media_size.js
@@ -1,14 +1,15 @@
-  const mediaSize = () => {
-      if (Modernizr.mq('(min-width: 986px)')) {
-          $('.responsive-carousel').addClass('is-4');
-          $('.responsive-carousel').removeClass('is-2');
-      } else {
-          $('.responsive-carousel').addClass('is-2');
-          $('.responsive-carousel').removeClass('is-4');
+const mediaSize = () => {
+      if(Modernizr.mq('(max-width: 600px)')) {
+        $('.responsive-carousel').addClass('is-1');
+        $('.responsive-carousel').removeClass('is-2 is-4');
+      } else if(Modernizr.mq('(max-width: 900px)')) {
+        $('.responsive-carousel').addClass('is-2');
+        $('.responsive-carousel').removeClass('is-1 is-4');
+      } else if(Modernizr.mq('(min-width: 1088px)')) {
+        $('.responsive-carousel').addClass('is-4');
+        $('.responsive-carousel').removeClass('is-1 is-2');
       }
   };
-
-let query = Modernizr.mq('(max-width: 900px)');
 
 export function responsiveResizing() {
   mediaSize();
