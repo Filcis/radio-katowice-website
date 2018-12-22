3972a4600f4dfd7b84c223d035d1980c7147a74b
Filcis <cislakfilip@gmail.com>
2018-12-22 18:09:45 +0100

    started working on better accordion

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -32,6 +32,39 @@ export function attachCarousels() {
 /* ==================================================
                    Swiper
 ================================================== */
+
+class rkAccordion {
+  constructor(selector) {
+    this.instance = $(selector);
+    this.accordionLinks = $(this.element).find('.accordion-title, navbar-link');
+    this.init();
+  }
+
+  static attach(selector = '.accordion, .navbar-menu') {
+    let instances = new Array();
+    const elements = document.querySelectorAll(selector);
+    [].forEach.call(elements, element => {
+      setTimeout(() => {
+        instances.push(new rkAccordion(element));
+      }, 100);
+    });
+    return instances;
+  }
+
+  init() {
+    this.accordionLinks.click()
+    console.log("accordion initiated");
+  }
+
+}
+
+export function accordions() {
+  var accordions = rkAccordion.attach();
+}
+
+/* ==================================================
+                   Swiper
+================================================== */
 class rkSwiper {
   constructor(selector) {
 
